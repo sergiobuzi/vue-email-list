47b791c25aee2637223527a863a8d0716104f142
SergioBuzi <sergio.buzi97@gmail.com>
2023-12-13 14:30:29 +0100

    config: setup base scaffolding

---
+++ script.js
@@ -0,0 +1,26 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+           
+
+        }
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+        console.log("l'applicazione e caricata");
+
+    }
+    
+}).mount('#app')
